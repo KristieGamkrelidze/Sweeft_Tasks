443429bb52d1947f24638c0c843e83653f8cd951
Kristine Gamkrelidze <77150992+KristieGamkrelidze@users.noreply.github.com>
2021-07-18 19:27:50 +0400

    Add files via upload

--- task1.php
+++ task1.php
@@ -1,27 +1,26 @@
 <?php
-// task 1
+// Task 1
 // This is a function that returns Fibonacci sequence.
 
 function fibonacci($int){
-    $n = 0;
     $int1 = 0; 
     $int2 = 1;
+    $int3 = 0;
     $resultArr = [];
     
     array_push($resultArr, $int1);
     array_push($resultArr, $int2);
 
-
-    while ($n < $int){  // keeps looping while $n is less than given int
+    while ($int3 < $int){  // keeps looping while $int is less than given int
         $int3 = $int2 + $int1; //the second and first numbers are added to define third number.
+        if($int3 <= $int){
         array_push($resultArr, $int3); 
+        }
         $int1 = $int2;   
         $int2 = $int3;  
         //there are shifts for the next iteration
-        $n = $n + 1; 
-        //increases by one for the next iteration
     }  
     print_r($resultArr);
 }
 
-fibonacci(6);
+fibonacci(10);
